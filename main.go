0e734c7ef4df1c5518f5528e286c60f2aa47ae08
andriylesch <andriy.lesch@ricardo.ch>
2018-05-06 23:56:26 +0200

    added draft endpoints

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-redis/redis"
+	"github.com/gorilla/mux"
 	"github.com/redis-golang/config"
 )
+
+type User struct {
+	UserID    int    `json:"user_id"`
+	Email     string `json:"email"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	UserType  string `json:"user_type"`
+}
 
 func main() {
 
@@ -16,13 +29,59 @@ func main() {
 		DB:       0,  // use default DB
 	})
 
-	pong, err := client.Ping().Result()
-
+	_, err := client.Ping().Result()
 	if err != nil {
 		fmt.Println("Error : ", err.Error())
 		os.Exit(1)
 	}
 
-	fmt.Println("Pong : ", pong)
+	r := mux.NewRouter()
 
+	r.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Write([]byte("Test data"))
+	})
+
+	r.HandleFunc("/user/{id}", func(w http.ResponseWriter, r *http.Request) {
+
+		var user User
+		err := json.NewDecoder(r.Body).Decode(&user)
+
+		// if error stop process
+		if err != nil {
+			fmt.Printf("Error during parse User model : %v", err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// Here can be your logic inserting data to DB or etc.
+
+		// Logic check if user olready exists in redis DB
+		userKey := fmt.Sprintf("userid_%d", user.UserID)
+
+		_, err = client.Get(userKey).Result()
+		if err == redis.Nil {
+			fmt.Println(userKey + " key does not exist")
+
+			// userKey not exists int this case will do insert data to Redis
+			err = client.Set(userKey, user, time.Minute).Err()
+			if err != nil {
+				fmt.Println("Error during insert data to redis : " + err.Error())
+			}
+		}
+
+		// In case if user was inserted in DB.
+		w.WriteHeader(http.StatusCreated)
+
+	}).Methods("POST")
+
+	sr := &http.Server{
+		Handler: r,
+		Addr:    "localhost:8081",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(sr.ListenAndServe())
 }
